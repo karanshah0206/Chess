ada275b0c360866c0f723184e6326ae7897c18ed
Karan Shah <karanshah0206@outlook.com>
2023-01-10 17:51:37 +0530

    Player Connection Management Backend

--- index.js
+++ index.js
@@ -9,3 +9,21 @@ const port = 80;
 
 app.use(express.static("public"));
 server.listen(port, () => {});
+
+/* GAME LOGIC STARTS HERE */
+let peerCount = 0;
+
+io.on("connection", (socket) => {
+  peerCount++;
+  if (peerCount > 2) socket.emit("full");
+  else if (peerCount == 1) socket.emit("firstJoined");
+  else {
+    socket.emit("secondJoined");
+    socket.broadcast.emit("opponentJoined");
+  }
+
+  socket.on("disconnect", () => {
+    peerCount--;
+    if (peerCount == 1) socket.broadcast.emit("opponentLeft");
+  });
+});

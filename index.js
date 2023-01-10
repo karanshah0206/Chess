5fe70639ce951a766b952c8c11c5fdae9059f4a5
Karan Shah <karanshah0206@outlook.com>
2023-01-10 17:06:57 +0530

    Serve Webpage on Port 80

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+const port = 80;
+
+app.use(express.static("public"));
+server.listen(port, () => {});
